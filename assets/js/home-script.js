582cb108251cd467b745dde922f460f99a05f176
User <nihatss@code.edu.az>
2024-10-17 12:32:52 +0400

    menu half done

---
+++ assets/js/home-script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let menu = document.querySelector("header .menu")
+
+menu.addEventListener("mouseover",function(){
+    this.classList.remove("d-none")
+})
+$('.single-item').slick();
+
+let headerBtn = document.querySelector(".category button");
+
+
