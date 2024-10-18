e459edcbe6226ed43de79b9c6cbdb6ccf900daf8
User <nihatss@code.edu.az>
2024-10-18 19:26:02 +0400

    home page 70% done

--- assets/js/home-script.js
+++ assets/js/home-script.js
@@ -9,4 +9,11 @@ $('.single-item').slick();
 
 let headerBtn = document.querySelector(".category button");
 
+let cardImg = document.querySelector("main #offer .card img")
 
+cardImg.addEventListener("mouseover",function(){
+    this.setAttribute("src","../assets/images/home-images/ford-gt50.webp")
+})
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
