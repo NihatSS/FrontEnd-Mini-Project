b5a4e389d94b6575a65b6c1ab944394b22f50620
User <nihatss@code.edu.az>
2024-10-19 18:35:15 +0400

    home page 90% done

--- assets/js/home-script.js
+++ assets/js/home-script.js
@@ -2,18 +2,15 @@
 
 let menu = document.querySelector("header .menu")
 
-menu.addEventListener("mouseover",function(){
-    this.classList.remove("d-none")
-})
-$('.single-item').slick();
-
-let headerBtn = document.querySelector(".category button");
-
-let cardImg = document.querySelector("main #offer .card img")
 
 cardImg.addEventListener("mouseover",function(){
     this.setAttribute("src","../assets/images/home-images/ford-gt50.webp")
 })
+var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
